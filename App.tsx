662df8f9f4feea97ed0405204b4f9b3e6ec9ca58
blueray00 <raynara.gustavo@ifrn.escolar.edu.br>
2026-03-30 23:38:15 -0300

    Commit inicial: borda e cabeçalho

--- App.tsx
+++ App.tsx
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.border}>
+        <Text style={styles.titulo}>REPÚBLICA FEDERATIVA DO BRASIL</Text>
+        <Text style={styles.subtitulo}>IDENTIDADE VISUAL</Text>
       <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -17,4 +20,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  border: {
+    width: 350,
+    height: 450,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: "red",
+    borderRadius: 20,
+    backgroundColor: "rgba(253, 5, 5, 0.3)",
+    justifyContent: "flex-start",
+  },
+  titulo: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "red",
+    textAlign: "center",
+  },
+  subtitulo: {
+    fontSize: 13,
+    color: "red",
+    textAlign: "center",
+  },
+
 });
